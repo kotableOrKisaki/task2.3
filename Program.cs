489ec9eddf523966fde6cd15c00eee7ca351a5df
kotableOrKisaki <super_misha05@mail.ru>
2022-08-27 23:28:02 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beginValue = 5;
+            int endValue = 96;
+
+            for (int value = beginValue; value <= endValue; value += 7)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
